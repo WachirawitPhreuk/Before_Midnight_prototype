230da797f970b8df8bae78356f046f2b0353d028
WachirawitPhreuk <wachirawit.pruk@mail.kmutt.ac.th>
2023-04-30 15:37:01 +0700

    Interact
    interact code

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRange;
+    public Button interactButton;
+    public UnityEvent interactAction;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactButton.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInRange) {
+            interactAction.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.CompareTag("Player")) {
+            isInRange = true;
+            interactButton.enabled = true;
+            Debug.Log("Player now in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if(collision.gameObject.CompareTag("Player")) {
+            isInRange = false;
+            interactButton.enabled = false;
+            Debug.Log("Player now not in range");
+        }
+    }
+}
