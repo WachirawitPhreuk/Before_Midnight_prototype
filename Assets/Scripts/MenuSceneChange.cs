33ffa0186e0620e90ae84cf5086457762314ff94
WachirawitPhreuk <wachirawit.pruk@mail.kmutt.ac.th>
2023-05-08 18:26:57 +0700

    Add Cover

--- Assets/Scripts/MenuSceneChange.cs
+++ Assets/Scripts/MenuSceneChange.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChange : MonoBehaviour
+public class MenuSceneChange : MonoBehaviour
 {
     public void ChangeScene() {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
 }
